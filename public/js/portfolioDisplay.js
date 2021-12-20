89d656daf3719d4be1b190326b77cb6564803b13
Rik Ghosh <rikghosh487@gmail.com>
2021-12-20 04:17:07 -0600

    Sliding feature for portfolio

---
+++ public/js/portfolioDisplay.js
@@ -0,0 +1,47 @@
+// get elements from document
+const buttons = document.querySelector('.buttons');
+const buttonList = buttons.children;
+const inContent = document.querySelector('.content').children;
+
+// compute grid columns dynamically
+buttons.style.gridTemplateColumns = `repeat(${buttonList.length}, 1fr)`;
+
+let currentIndex;
+let timeout;
+const swapCallbackTime = 500;
+
+const swap = index => {
+    if(index == currentIndex) return;           // no change needed
+
+    // update button colors
+    buttonList[currentIndex].classList.remove('current');
+    buttonList[index].classList.add('current');
+
+    clearTimeout(timeout);
+    if(index < currentIndex) {                  // shifting left
+        inContent[currentIndex].classList.add('swipe-left');
+        timeout = setTimeout(() => {
+            inContent[index].classList.remove('hide');
+            inContent[currentIndex].classList.remove('swipe-left');
+            inContent[currentIndex].classList.add('hide');
+            currentIndex = index;
+        }, swapCallbackTime);
+    } else {                                    // shifting right
+        inContent[currentIndex].classList.add('swipe-right');
+        timeout = setTimeout(() => {
+            inContent[index].classList.remove('hide');
+            inContent[currentIndex].classList.remove('swipe-right');
+            inContent[currentIndex].classList.add('hide');
+            currentIndex = index;
+        }, swapCallbackTime);
+    }
+};
+
+// find the current button and register event handlers
+for(let i = 0; i < buttonList.length; i++) {
+    if(buttonList[i].classList.contains('current')) currentIndex = i;
+    buttonList[i].addEventListener('click', e => {
+        e.preventDefault();
+        swap(i);
+    });
+}
